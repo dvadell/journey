323eb4adfddcafcb1f87f1c06af9e905d1049970
Diego M. Vadell <diego.m.vadell@gmail.com>
2025-01-30 23:33:36 +0000

    Add tests for lib/home_page.dart

---
+++ test/lib_home_page_test.dart
@@ -0,0 +1,19 @@
+// Import necessary packages
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'lib/home_page.dart';
+
+void main() {
+  group('JournalHome', () {
+    testWidgets('should have a title', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: JournalHome()));
+      expect(find.text('Journal Home'), findsOneWidget);
+    });
+
+    testWidgets('should have tabs', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: JournalHome()));
+      expect(find.byType(TabBar), findsOneWidget);
+      expect(find.byType(TabBarView), findsOneWidget);
+    });
+  });
+}
